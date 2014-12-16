cd181056860fcd7f0925ad8e2ea6a2500d537c73
tophat8855 <hrfarley@gmail.com>
2014-12-16 10:07:39 -0800

    reworked challenge 2, maybe it's not really working?

--- grading.rb
+++ grading.rb
@@ -1,5 +1,4 @@
-array = [10, 9, 8]
-
+array =[10, 9, 8]
 index = 0
 differences = []
 
@@ -16,21 +15,37 @@ end
 
 p differences
 
-count = 0
-ind = 0
-while ind < (differences.length)
-  if differences[ind] == :down
-    count += 1
-  elsif differences[ind] == :up
-    count -= 1
-  else differences[ind] == :even
-    count
-  end
-  ind += 1
+array_index = 0
+number_of_downs = 0
+
+if differences.last == :up || differences.last == :even || differences.length < 3
+  puts "not in decline"
+else
+  # while (array_index < (differences.length - 1)) && (number_of_downs < 3)
+  #   if differences[array_index] == :down
+  #     number_of_downs += 1
+  #   end
+  #   array_index += 1
+  # end
+  puts "in decline"
 end
 
-if count >= 3
-  puts "in decline"
-else
-  puts "not in decline"
-end
+
+# count = 0
+# ind = 0
+# while ind < (differences.length)
+#   if differences[ind] == :down
+#     count += 1
+#   elsif differences[ind] == :up
+#     count -= 1
+#   else differences[ind] == :even
+#     count
+#   end
+#   ind += 1
+# end
+
+# if count >= 3
+#   puts "in decline"
+# else
+#   puts "not in decline"
+# end
